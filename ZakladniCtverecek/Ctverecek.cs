509d4385a22b771684628fb8d4dca0b2ced26043
Jakub Šenkýř <senkyr@spstrutnov.cz>
2018-12-04 11:40:37 +0100

    Základní čtvereček

---
+++ ZakladniCtverecek/Ctverecek.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace ZakladniCtverecek
+{
+    class Ctverecek
+    {
+        public int Velikost { get; private set; }
+        public int Rychlost { get; private set; }
+
+        public Color Barva;
+        public Vector2 Pozice;
+
+        private Texture2D textura;
+
+        public Keys SmerNahoru { get; private set; }
+        public Keys SmerDolu { get; private set; }
+        public Keys SmerDoleva { get; private set; }
+        public Keys SmerDoprava { get; private set; }
+
+        public Ctverecek(int velikost, int rychlost, Color barva, float poziceX, float poziceY, GraphicsDevice zobrazovaciZarizeni, Keys nahoru, Keys dolu, Keys doleva, Keys doprava)
+        {
+            Velikost = velikost;
+            Rychlost = rychlost;
+
+            Barva = barva;
+
+            Pozice.X = poziceX;
+            Pozice.Y = poziceY;
+
+            #region Priprava textury
+            textura = new Texture2D(zobrazovaciZarizeni, Velikost, Velikost);
+
+            Color[] barevnaData = new Color[Velikost * Velikost];
+
+            for (int i = 0; i < barevnaData.Length; i++)
+                barevnaData[i] = Color.White;
+
+            textura.SetData(barevnaData);
+            #endregion
+
+            SmerDoleva = doleva;
+            SmerDoprava = doprava;
+            SmerNahoru = nahoru;
+            SmerDolu = dolu;
+        }
+
+        public void aktualizovat()
+        {
+            if (Keyboard.GetState().IsKeyDown(SmerNahoru))
+                Pozice.Y -= Rychlost;
+            if (Keyboard.GetState().IsKeyDown(SmerDolu))
+                Pozice.Y += Rychlost;
+            if (Keyboard.GetState().IsKeyDown(SmerDoleva))
+                Pozice.X -= Rychlost;
+            if (Keyboard.GetState().IsKeyDown(SmerDoprava))
+                Pozice.X += Rychlost;
+        }
+
+        public void vykreslit(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(textura, Pozice, Barva);
+        }
+    }
+}
---
+++ ZakladniCtverecek/Hra.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace ZakladniCtverecek
+{
+    public class Hra : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        int sirkaOkna = 800;
+        int vyskaOkna = 600;
+
+        Ctverecek ctverecek;
+
+        public Hra()
+        {
+            graphics = new GraphicsDeviceManager(this);
+        }
+
+        protected override void Initialize()
+        {
+            graphics.PreferredBackBufferWidth = sirkaOkna;
+            graphics.PreferredBackBufferHeight = vyskaOkna;
+            graphics.ApplyChanges();
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            int velikostCtverecku = 50;
+            int rychlostCtverecku = 5;
+
+            ctverecek = new Ctverecek(velikostCtverecku, rychlostCtverecku, Color.Black, (sirkaOkna - velikostCtverecku) / 2, (vyskaOkna - velikostCtverecku) / 2, GraphicsDevice, Keys.Up, Keys.Down, Keys.Left, Keys.Right);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            ctverecek.aktualizovat();
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.White);
+
+            spriteBatch.Begin();
+            ctverecek.vykreslit(spriteBatch);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
